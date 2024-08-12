e63334d17ec076e831c8264b0d9ae79c67aebbf1
Abdullah al rafi <abdullah116632@gmail.com>
2024-08-12 09:33:01 +0600

    home page & services

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,50 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
+import Stats from "@/components/Stats";
+
+const Home = () => {
   return (
-    <main>
-      Homepage
-    </main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-1 xl:pb-8">
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Web Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />
+              <span className="text-accent text-[48px] xl:text-[65px]">Abdullah al rafi</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am
+              proficiences in various programming languages and techonologies{" "}
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+      <Stats />
+    </section>
   );
-}
+};
+
+export default Home;
--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ import MovileNav from "./MovileNav";
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white">
+    <header className="py-6 xl:py-8 text-white">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
